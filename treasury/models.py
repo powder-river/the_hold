6e328cc3268c041590a96eb597f29e8949c7c88f
Danai Adkisson <dadkisso@live.com>
2016-06-04 20:14:35 -0400

    add treasury objects

--- treasury/models.py
+++ treasury/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Creditor(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.EmailField()
+    contact_person = models.CharField(max_length=200)
+
+class Debt(models.Model):
+    name = models.CharField(max_length=200)
+    occurance = models.DurationField
+    amount = models.DecimalField(max_digits=20,decimal_places=2)
